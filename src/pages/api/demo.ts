095db3b871d0d70fe511285293cf4a1062748ce8
C. Lewis <ctj.lewis@icloud.com>
2023-02-24 21:50:57 -0600

    chore: add libraries and demo

---
+++ src/pages/api/demo.ts
@@ -0,0 +1,29 @@
+import { type NextRequest } from "next/server";
+import { OpenAI } from "openai-streams";
+
+export default async function demo (req: NextRequest) {
+  const { name } = await req.json();
+  if (typeof name !== "string") {
+    return new Response(null, { status: 400, statusText: "Did not include `name` parameter" });
+  }
+
+  const completionsStream = await OpenAI(
+    // "edits",
+    "completions",
+    {
+      // model: "text-davinci-edit-001",
+      // input: "What day of the wek is it?",
+      // instruction: "Fix the spelling mistakes",
+      model: "text-davinci-003",
+      prompt: `Write a nice two-sentence paragraph about a person named ${name}.\n\n`,
+      temperature: 1,
+      max_tokens: 100
+    }
+  );
+
+  return new Response(completionsStream);
+}
+
+export const config = {
+  runtime: "edge"
+};
